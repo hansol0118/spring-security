0f1c698a9891a5be41f0d9bb4eb96639888081d8
hansol0118 <wall197944@naver.com>
2026-03-30 11:21:05 +0900

    23, 24

--- src/main/java/com/back/domain/member/entity/Member.java
+++ src/main/java/com/back/domain/member/entity/Member.java
@@ -6,32 +6,26 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Entity
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
 public class Member extends BaseEntity {
+
     @Column(unique = true)
     private String username;
-
     private String password;
-
     private String nickname;
-
     @Column(unique = true)
     private String apiKey;
 
-    public Member(String username, String password, String nickname){
-        this.username =username;
+    public Member(String username, String password, String nickname, String apiKey) {
+        this.username = username;
         this.password = password;
         this.nickname = nickname;
-        this.apiKey = UUID.randomUUID().toString();
+        this.apiKey = apiKey;
     }
 
     public String getName() {
         return nickname;
     }
-
-
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -25,41 +25,41 @@ public class BaseInitData {
     @Bean
     public ApplicationRunner initData() {
         return args -> {
-            self.work2();
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
-    public void work1() {
-        if(postService.count() > 0) {
+    public void work2() {
+        if (postService.count() > 0) {
             return;
         }
 
         Member author1 = memberService.findByUsername("user1").get();
         Member author2 = memberService.findByUsername("user2").get();
 
-        Post post1 = postService.write(author1,"제목1", "내용1");
+        Post post1 = postService.write(author1, "제목1", "내용1");
         Post post2 = postService.write(author1, "제목2", "내용2");
         postService.write(author2, "제목3", "내용3");
 
-        post1.addComment(author1,"댓글 1-1");
-        post1.addComment(author1,"댓글 1-2");
-        post1.addComment(author1,"댓글 1-3");
-        post2.addComment(author2,"댓글 2-1");
-        post2.addComment(author2,"댓글 2-2");
+        post1.addComment(author1, "댓글 1-1");
+        post1.addComment(author1, "댓글 1-2");
+        post1.addComment(author1, "댓글 1-3");
+        post2.addComment(author2, "댓글 2-1");
+        post2.addComment(author2, "댓글 2-2");
     }
 
     @Transactional
-    public void work2() {
-        if(memberService.count() > 0) {
+    public void work1() {
+        if (memberService.count() > 0) {
             return;
         }
-        memberService.join("system", "system", "시스템");
-        memberService.join("admin", "admin", "운영자");
-        memberService.join("user1", "1234", "유저1");
-        memberService.join("user2", "1234", "유저2");
-        memberService.join("user3", "1234", "유저3");
 
+        memberService.join("system", "system", "시스템", "system");
+        memberService.join("admin", "admin", "운영자", "admin");
+        memberService.join("user1", "1234", "유저1", "user1");
+        memberService.join("user2", "1234", "유저2", "user2");
+        memberService.join("user3", "1234", "유저3", "user3");
     }
 }
