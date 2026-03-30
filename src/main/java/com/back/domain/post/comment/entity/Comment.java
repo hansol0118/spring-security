af033f23ffc8a5939a581600f30e4c4591a4cb10
hansol0118 <wall197944@naver.com>
2026-03-30 14:21:40 +0900

    30

--- src/main/java/com/back/domain/post/comment/entity/Comment.java
+++ src/main/java/com/back/domain/post/comment/entity/Comment.java
@@ -3,6 +3,7 @@ package com.back.domain.post.comment.entity;
 import com.back.domain.member.entity.Member;
 import com.back.domain.post.post.entity.Post;
 import com.back.global.entity.BaseEntity;
+import com.back.global.exception.ServiceException;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -28,8 +29,19 @@ public class Comment extends BaseEntity {
         this.post = post;
     }
 
-
     public void update(String content) {
         this.content = content;
+    }
+
+    public void checkActorModify(Member actor) {
+        if(!this.author.getId().equals(actor.getId())) {
+            throw new ServiceException("403-1", "댓글 수정 권한이 없습니다.");
+        }
+    }
+
+    public void checkActorDelete(Member actor) {
+        if(!this.author.getId().equals(actor.getId())) {
+            throw new ServiceException("403-2", "댓글 삭제 권한이 없습니다.");
+        }
     }
 }
