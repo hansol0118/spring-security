b60cc2ff5ad72c894ad90e52b6023b2db1941faa
hansol0118 <wall197944@naver.com>
2026-04-01 11:28:40 +0900

    61-1

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +19,70 @@ public class Rq {
     private final HttpServletRequest request; // requestScope
     private final HttpServletResponse response;
     private final MemberService memberService;
+
+
+    public Member getActor() {
+
+        String authorizationHeader = getHeader("Authorization", "");
+
+        String apiKey;
+        String accessToken;
+        //
+        if (authorizationHeader != null) {
+//            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+            if (!authorizationHeader.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "잘못된 형식의 인증데이터입니다.");
+            }
+            String[] headerAuthorizationBits = authorizationHeader.split(" ", 3);
+            apiKey = authorizationHeader.replace("Bearer ", "");
+            apiKey = headerAuthorizationBits[1];
+            accessToken = headerAuthorizationBits.length == 3 ? headerAuthorizationBits[2] : "";
+        } else {
+            apiKey = getCookieValue("apiKey", "");
+            accessToken = getCookieValue("accessToken", "");
+        }
+
+        if (apiKey.isBlank()) {
+            throw new ServiceException("401-3", "인증 정보가 존재하지 않습니다.");
+        }
+
+
+        return memberService.findByApiKey(apiKey).orElseThrow(
+                () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
+        );
+    }
+
+
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
+    private String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
+    }
+
+    public void deleteCookie(String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
+        cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+    }
 
     public void addCookie(String name, String value) {
         Cookie cookie = new Cookie(name, value);
@@ -28,50 +93,6 @@ public class Rq {
         response.addCookie(
                 cookie
         );
-    }
-
-
-    public Member getActor() {
-
-        String authorizationHeader = request.getHeader("Authorization");
-
-        String apiKey;
-        //
-        if(authorizationHeader != null) {
-//            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
-            if(!authorizationHeader.startsWith("Bearer ")) {
-                throw new ServiceException("401-2", "잘못된 형식의 인증데이터입니다.");
-            }
-            apiKey = authorizationHeader.replace("Bearer ", "");
-        }
-        else{
-            apiKey = request.getCookies() == null ? ""
-                    : Arrays.stream(request.getCookies())
-                    .filter(cookie -> cookie.getName().equals("apiKey"))
-                    .map(cookie -> cookie.getValue())
-                    .findFirst()
-                    .orElse("");
-        }
-
-        if (apiKey.isBlank()){
-            throw new ServiceException("401-3","인증 정보가 존재하지 않습니다.");
-        }
-
-
-        return memberService.findByApiKey(apiKey).orElseThrow(
-                () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
-        );
-    }
-
-
-    public void deleteCookie(String name) {
-        Cookie cookie = new Cookie(name, "");
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setDomain("localhost");
-        cookie.setMaxAge(0);
-
-        response.addCookie(cookie);
     }
 
 }
